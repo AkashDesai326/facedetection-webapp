e2da131635ef303fa7b3342e2dafddc3f3798ccc
Tejas Chauhan <tejas.chauhan@kevit.io>
2021-03-28 22:36:52 +0530

    Added Recognition models

--- recognition/admin.py
+++ recognition/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Attendance, Recognition
 
 # Register your models here.
+class AttedanceModel(admin.ModelAdmin):
+    pass
+
+class RecognitionModel(admin.ModelAdmin):
+    pass
+
+admin.site.register(Attendance, AttedanceModel)
+admin.site.register(Recognition, RecognitionModel)
--- recognition/models.py
+++ recognition/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recognition(models.Model):
+    sId = models.IntegerField(primary_key=True)
+    requestTime = models.DateTimeField()
+
+class Attendance(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    inTime = models.DateTimeField()
+    outTime = models.DateTimeField()
