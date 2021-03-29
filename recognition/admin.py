049a7d3855bdc32c9ffe329217f4a99508ae71cc
Tejas Chauhan <tejas.chauhan@kevit.io>
2021-03-29 17:28:32 +0530

    get details from database

--- recognition/admin.py
+++ recognition/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
-from .models import Attendance, Recognition
+from .models import Attendance, Recognition, CameraMonitor, Student
+
 
 # Register your models here.
 class AttedanceModel(admin.ModelAdmin):
     pass
 
+
 class RecognitionModel(admin.ModelAdmin):
     pass
 
+
+class CameraMonitoring(admin.ModelAdmin):
+    pass
+
+
+class StudentDetails(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Attendance, AttedanceModel)
 admin.site.register(Recognition, RecognitionModel)
+admin.site.register(CameraMonitor, CameraMonitoring)
+admin.site.register(Student, StudentDetails)
--- recognition/urls.py
+++ recognition/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from recognition import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('capture_image', views.capture_image, name='capture_image'),
     path('train_image', views.train_image, name='train_image'),
     path('track_image', views.track_my_image, name='track_image'),
-    path('trackimage',views.trackimage, name='trackimage')
-    ]
+    path('trackimage', views.trackimage, name='trackimage'),
+    path('closecamera', views.close_camera, name="closecamera"),
+]
